442b1e790ad79caa83c3e3c4bb10e04a8036fbc5
Mehmet Akif Salman <ahmetakifsalman@gmail.com>
2024-07-29 09:03:32 +0300

    Backend added

--- library-app/front-end/src/App.tsx
+++ library-app/front-end/src/App.tsx
@@ -1,8 +1,4 @@
 import './App.css';
-import Carousel from './Layouts/HomePage/components/Carousel';
-import ExploreTopBooks from './Layouts/HomePage/components/ExploreTopBooks';
-import Heros from './Layouts/HomePage/components/Heros';
-import LibraryServices from './Layouts/HomePage/components/LibraryServices';
 import HomePage from './Layouts/HomePage/HomePage';
 import Footer from './Layouts/NavbarAndFooter/Footer';
 import Navbar from './Layouts/NavbarAndFooter/Navbar';
